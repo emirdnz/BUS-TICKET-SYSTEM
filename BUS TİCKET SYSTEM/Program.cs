8471dd3f34771bf885aa73ca46733bb536a266b1
polegut <polegut8@gmail.com>
2023-07-30 06:47:57 +0300

    Add project files.

---
+++ BUS TİCKET SYSTEM/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BUS_TİCKET_SYSTEM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
